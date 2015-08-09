b1556017ce17e03b536ecb34f841fe13dd7e1826
Espen Gulbrandsen <espengulbrandsen@Espens-MacBook-Pro.local>
2015-08-09 16:35:50 +0200

    Add simple Chord class

---
+++ Builds/MacOSX/Chord.cpp
@@ -0,0 +1,22 @@
+//
+//  Chord.cpp
+//  Test
+//
+//  Created by Espen Gulbrandsen on 09/08/15.
+//
+//
+
+#include "Chord.h"
+
+
+
+
+Chord::Chord(int intervals[])
+{
+    intervals = intervals;
+}
+
+Chord::Chord()
+{
+    
+}
---
+++ Builds/MacOSX/Chord.h
@@ -0,0 +1,24 @@
+//
+//  Chord.h
+//  Test
+//
+//  Created by Espen Gulbrandsen on 09/08/15.
+//
+//
+
+#ifndef __Test__Chord__
+#define __Test__Chord__
+
+#include <stdio.h>
+class Chord {
+    
+    int intervals[];
+    
+    
+public:
+    Chord(int intervals[]);
+    Chord();
+};
+
+
+#endif /* defined(__Test__Chord__) */
